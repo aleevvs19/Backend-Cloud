fb152c5aa29ec009a9009f81d10fc59937bb0909
Alesalazarv <ale.salazarv@duocuc.cl>
2026-09-14 09:18:42 -0300

    Add files via upload

---
+++ pedidos360-backend-notificaciones/pedidos360-backend-notificaciones/src/test/java/com/pedidos360/notificaciones/Pedidos360BackendNotificacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedidos360.notificaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pedidos360BackendNotificacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
